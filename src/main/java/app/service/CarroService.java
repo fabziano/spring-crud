82cd74a62e01feb39c68449d7e47a1199ce26e39
Fabiano <fabianotmp@gmail.com>
2024-08-12 16:39:27 -0300

    backup

--- src/main/java/app/service/CarroService.java
+++ src/main/java/app/service/CarroService.java
@@ -2,11 +2,33 @@ package app.service;
 
 import org.springframework.stereotype.Service;
 
+import app.entity.Carro;
+
 @Service
 public class CarroService {
 
 	public String salvarCarro(Carro carro) {
-		
+		//implementar
+		return "Carro adicionado com sucesso";
+	}
+	
+	public String alterarCarro(Carro carro, long id) {
+		//implementar
+		return "Carro alterado com sucesso";
+	}
+	
+	public String deletarCarro(long id) {
+		//implementar
+		return "Carro deletado com sucesso";
+	}
+	
+	public List<Carro> ListarCarros(){
+		//implementar
+		return null;
+	}
+	public carro buscarPorId(long id) {
+		//implementar
+		return null;
 	}
 
 }
